b2668d1f53bf61ea04ded390d931ef649b93a60a
kirubel141797 <kirubelteshome22@gmail.com>
2023-09-20 14:50:50 -0700

    putchar

---
+++ _putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+* _putchar - To write a character c to stdout
+* @c: A character to be  printed
+* @p: A pointer to be updated in the length
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c, int *p)
+{
+	*p += 1;
+
+	return (write(1, &c, 1));
+}
