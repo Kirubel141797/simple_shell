1aa8899aa17053a7caf62928e862b18794030484
kirubel141797 <kirubelteshome22@gmail.com>
2023-09-20 14:36:46 -0700

    Print_integer

---
+++ Print_integer.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+  * print_Number - print a decimal and integer
+  * @n: numbers to be printed
+  * @p: a pointer to determine length
+  *
+  * Return: Number of digits of n
+  */
+int print_Number(int n, int *p)
+{
+	unsigned int new = n;
+	int cont;
+
+	if (n < 0)
+	{
+		_putchar('-', p);
+		new *= -1;
+		cont = 1;
+	}
+	if (new / 10)
+	{
+		print_Number((new / 10), p);
+	}
+	_putchar(((new % 10) + '0'), p);
+	while (new > 10)
+	{
+		new /= 10;
+		cont++;
+	}
+	cont++;
+	return (cont);
+}
